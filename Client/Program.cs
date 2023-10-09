96124f355a18e8f02ff7ca630800645de095cb4b
Daitarus <19fof91@gmail.ru>
2023-10-09 17:35:47 +0300

    Add test client get request for google

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public static class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var myClient = new MyClient();
+            Console.WriteLine("Приложение начало работу");
+            await myClient.Start();
+            Console.WriteLine("Приложение завершило работу");
+        }
+    }
+
+    public class MyClient
+    {
+        public async Task Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                using (var client = new HttpClient())
+                {
+                    using var result = await client.GetAsync("https://google.com");
+                    Console.WriteLine(result.StatusCode);
+                }
+            }
+        }
+    }
+}
